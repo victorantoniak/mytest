601053c018d885d398fdaa50348adcb0654deb45
victorantoniak <victorantoniak@gmail.com>
2015-06-07 15:20:09 +0300

    c

--- ordernetto.php
+++ ordernetto.php
@@ -1,16 +1,15 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 
-class plgJshoppingCheckoutOrderNetto extends JPlugin
-{
+class plgJshoppingCheckoutOrderNetto extends JPlugin{
     
-    function onLoadCheckoutStep5(){
+    public function onLoadCheckoutStep5(){
         updateAllprices();
 
     }
     
-    function onLoadStep5save(){
-        $jshopConfig = &JSFactory::getConfig();
+    public function onLoadStep5save(){
+        $jshopConfig = JSFactory::getConfig();
         $jshopConfig->display_price_front_current = 1;
         updateAllprices();
     }
